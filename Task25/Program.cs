f10aeb6915bf99ed10d9d37017474ac11a04585d
Mariya Nemzorova <nemzorova777@mail.ru>
2023-01-19 21:15:40 +0700

    Solve task 25

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+Console.Write("Введите число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите степень: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberB >= 0)
+{
+    double result = Power(numberA, numberB);
+    Console.Write($"{numberA} в степени {numberB} = {result}");
+}
+else Console.Write("Введена отрицательная степень");
+
+double Power(int numA, int numB)
+{
+    double res = Math.Pow(numA, numB);
+    return res;
+}
