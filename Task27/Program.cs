806911de68ae84169f8095e62b8e451b8bca265c
Mariya Nemzorova <nemzorova777@mail.ru>
2023-01-19 23:01:40 +0700

    Solve task 27

--- Task27/Program.cs
+++ Task27/Program.cs
@@ -2,3 +2,21 @@
 //452 -> 11
 //82 -> 10
 //9012 -> 12
+
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int resultSum = Sum(number);
+Console.Write(resultSum);
+
+int Sum(int num, int result = 0, int i = 0)
+{
+    while (num > 0)
+    {
+        i = num % 10;
+        result = i + result;
+        num = num / 10;
+    }
+    return result;
+}
